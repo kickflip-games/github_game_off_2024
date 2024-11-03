d2d0a40775346e9d00fe63f2c9a452357eed4600
ApokryphaV1 <zahraoui.elmehdi@gmail.com>
2024-11-03 15:51:44 +1300

    fixed glass and tilemap.

--- glass/glass.gd
+++ glass/glass.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
 @onready var glass_sprite:Sprite2D = $Sprite2D
-@onready var glass_fx:CPUParticles2D = $GlassFx
+@onready var glass_FX:CPUParticles2D = $GlassFx
+@onready var enter_id:int = 0
 
 func _on_body_entered(body):
-		glass_sprite.visible = false
-		glass_fx.emitting = true
-		queue_free()
+		enter_id += 1 
+		if enter_id == 1:
+			print("Body first time entered")
+			glass_sprite.visible = false
+			glass_FX.emitting = true
 		
+		#queue_free()
