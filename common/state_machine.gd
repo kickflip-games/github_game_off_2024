c5b89d33f1203169635e35a1e593a7772e81345d
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-11-04 12:16:04 +1300

    undo change for states

--- common/state_machine.gd
+++ common/state_machine.gd
@@ -2,50 +2,30 @@ class_name StateMachine extends Node
 
 @export var initial_state: State = null
 
-@onready var state: State = null
+@onready var state: State = (func get_initial_state() -> State:
+	return initial_state if initial_state != null else get_child(0)
+).call()
 
-# # Got an error of initialisation while trying to play
-# @onready var state: State = (func get_initial_state() -> State:
-# 	return initial_state if initial_state != null else get_child(0)
-# ).call()
-
-#func _ready() -> void:
-	#for state_node: State in find_children("*", "State"):
-		#state_node.finished.connect(_transition_to_next_state)
-#
-	#await owner.ready
-	#state.enter("")
 
 func _ready() -> void:
-	state = get_initial_state()
-	if state:
-		state.enter("")
-	else:
-		push_error("State not initialised")
+	for state_node: State in find_children("*", "State"):
+		state_node.finished.connect(_transition_to_next_state)
 
-func get_initial_state() -> State:
-	return initial_state if initial_state != null else get_child(0) as State
-	
+	await owner.ready
+	state.enter("")
+
 
 func _unhandled_input(event: InputEvent) -> void:
-	if state:
-		state.handle_input(event)
-	else:
-		push_error("State is null in _unhandled_input")
+	state.handle_input(event)
 
 
 func _process(delta: float) -> void:
-	if state:
-		state.update(delta)
-	else:
-		push_error("State is null in _process")
+	state.update(delta)
 
 
 func _physics_process(delta: float) -> void:
-	if state:
-		state.physics_update(delta)
-	else:
-		push_error("State is null in _physics_process")
+	state.physics_update(delta)
+
 
 func _transition_to_next_state(target_state_path: String, data: Dictionary = {}) -> void:
 	if not has_node(target_state_path):
--- enemy/states/idle.gd
+++ enemy/states/idle.gd
@@ -4,22 +4,11 @@ extends EnemyState
 @onready var timer:Timer = $IdleTimer
 
 
-# func enter(previous_state_path: String, data := {}) -> void:
-# 	enemy.animation.modulate = Color.WHITE
-# 	enemy.velocity = Vector2.ZERO
-# 	timer.start(enemy.idling_time_before_flipping)
-# 	#enemy.animation_player.play("idle")
-
 func enter(previous_state_path: String, data := {}) -> void:
-	if enemy == null:
-		enemy = get_parent() as Enemy  # or however you need to retrieve it
-	if enemy:
-		enemy.animation.modulate = Color.WHITE
-		enemy.velocity = Vector2.ZERO
-		timer.start(enemy.idling_time_before_flipping)
-		# enemy.animation_player.play("idle")
-	else:
-		push_error("Enemy instance is not set for IdleState.")
+	enemy.animation.modulate = Color.WHITE
+	enemy.velocity = Vector2.ZERO
+	timer.start(enemy.idling_time_before_flipping)
+	#enemy.animation_player.play("idle")
 
 
 func _on_idle_timer_timeout():
