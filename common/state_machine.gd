16fd1be7cea5fc00cef05861aa821e7ac630cb4b
Pichun <pichugin.mikhail@gmail.com>
2024-11-04 16:58:06 +0100

    added killzone + organized/cleaned main scene

--- common/state_machine.gd
+++ common/state_machine.gd
@@ -2,36 +2,56 @@ class_name StateMachine extends Node
 
 @export var initial_state: State = null
 
-@onready var state: State = (func get_initial_state() -> State:
-	return initial_state if initial_state != null else get_child(0)
-).call()
+@onready var state: State = null
 
+# # Got an error of initialisation while trying to play
+# @onready var state: State = (func get_initial_state() -> State:
+#   return initial_state if initial_state != null else get_child(0)
+# ).call()
+
+#func _ready() -> void:
+  #for state_node: State in find_children("*", "State"):
+	#state_node.finished.connect(_transition_to_next_state)
+#
+  #await owner.ready
+  #state.enter("")
 
 func _ready() -> void:
-	for state_node: State in find_children("*", "State"):
-		state_node.finished.connect(_transition_to_next_state)
+	state = get_initial_state()
+	if state:
+		state.enter("")
+	else:
+		push_error("State not initialised")
 
-	await owner.ready
-	state.enter("")
-
+func get_initial_state() -> State:
+	return initial_state if initial_state != null else get_child(0) as State
+  
 
 func _unhandled_input(event: InputEvent) -> void:
-	state.handle_input(event)
+	if state:
+		state.handle_input(event)
+	else:
+		push_error("State is null in _unhandled_input")
 
 
 func _process(delta: float) -> void:
-	state.update(delta)
+	if state:
+		state.update(delta)
+	else:
+		push_error("State is null in _process")
 
 
 func _physics_process(delta: float) -> void:
-	state.physics_update(delta)
-
+	if state:
+		state.physics_update(delta)
+	else:
+		push_error("State is null in _physics_process")
 
 func _transition_to_next_state(target_state_path: String, data: Dictionary = {}) -> void:
 	if not has_node(target_state_path):
 		printerr(owner.name + ": Trying to transition to state " + target_state_path + " but it does not exist.")
-		return
-
+	return
+	
 	var previous_state_path := state.name
 	state.exit()
 	state = get_node(target_state_path)
--- enemy/states/idle.gd
+++ enemy/states/idle.gd
@@ -4,25 +4,36 @@ extends EnemyState
 @onready var timer:Timer = $IdleTimer
 
 
+# func enter(previous_state_path: String, data := {}) -> void:
+#   enemy.animation.modulate = Color.WHITE
+#   enemy.velocity = Vector2.ZERO
+#   timer.start(enemy.idling_time_before_flipping)
+#   #enemy.animation_player.play("idle")
+
 func enter(previous_state_path: String, data := {}) -> void:
-	enemy.animation.modulate = Color.WHITE
-	enemy.velocity = Vector2.ZERO
-	timer.start(enemy.idling_time_before_flipping)
-	#enemy.animation_player.play("idle")
+  if enemy == null:
+    enemy = get_parent() as Enemy  # or however you need to retrieve it
+  if enemy:
+    enemy.animation.modulate = Color.WHITE
+    enemy.velocity = Vector2.ZERO
+    timer.start(enemy.idling_time_before_flipping)
+    # enemy.animation_player.play("idle")
+  else:
+    push_error("Enemy instance is not set for IdleState.")
 
 
 func _on_idle_timer_timeout():
-	enemy.flip_direction()
-	if enemy.patrollingEnemy:
-		finished.emit(PATROL)
+  enemy.flip_direction()
+  if enemy.patrollingEnemy:
+    finished.emit(PATROL)
 
 
 func update(_delta: float) -> void:
-	if enemy.player_is_visible():
-		finished.emit(ALERTED)
-	if enemy.isDead:
-		finished.emit(DEATH)
+  if enemy.player_is_visible():
+    finished.emit(ALERTED)
+  if enemy.isDead:
+    finished.emit(DEATH)
 
 
 func exit() -> void:
-	timer.stop()
+  timer.stop()
