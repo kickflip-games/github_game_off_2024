f117bde5aa2dadd0244773fed603021d03451de6
nsamelson <salocin.sam@gmail.com>
2024-11-04 14:24:38 +0100

    added hooked state but not working yet

--- player/states/grappling.gd
+++ player/states/grappling.gd
@@ -2,9 +2,6 @@ extends PlayerState
 # Grappling
 
 
-# TODO: if reaches the end of the grapplign hook, then move to 'HOOKED' state 
-# (player is attached to end of grapplingn hook)
-
 var chain_velocity: Vector2 = Vector2.ZERO
 
 var chain_direction:Vector2:
@@ -22,16 +19,9 @@ func handle_input(_event: InputEvent) -> void:
 
 
 
-func release_chain_and_transition_state():
-	player.chain.release()
-	if player.is_on_floor():
-		finished.emit(GROUNDED)
-	else:
-		finished.emit(AIRBORNE)
-
 ## Called by the state machine on the engine's physics update tick.
 func physics_update(_delta: float) -> void:
-	var input_direction_x := Input.get_axis("move_left", "move_right")
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	chain_velocity =  chain_direction * -player.CHAIN_PULL
 	chain_velocity.y *= 0.55 if chain_velocity.y > 0.0 else 1.65
@@ -48,11 +38,14 @@ func physics_update(_delta: float) -> void:
 	
 	elif d <= player.AT_HOOK_TIP_DISTANCE:
 		# Dampen only if not wanting to swing
-		if input_direction_x ==0:
-			player.velocity *= 0.1
+		if input_direction.length() ==0:
+			player.velocity *= 0.3
+			
+			#finished.emit(HOOKED,{"chain_velocity":chain_velocity}) #TODO: continue here and get better transition
 	
-	if input_direction_x !=0:
-		player.velocity.x +=  sign(input_direction_x)*  player.SWING_FORCE
+	if input_direction.length() !=0:
+		player.velocity.x += sign(input_direction.x) * player.SWING_FORCE
+		player.velocity.y += sign(input_direction.y) * player.SWING_FORCE * 5
 
 		
 	if player.chain.hooked:
--- player/states/player_state.gd
+++ player/states/player_state.gd
@@ -5,8 +5,23 @@ const AIRBORNE = "Airborne"
 const GRAPPLING = "Grappling"
 const HOOKED = "Hooked"
 
+#TODO: add these other states
+const IDLE = "Idle"
+const DANCING = "Dancing"
+const STUNNED = "Stunned"
+const INJURED = "Injured" # or hit?
+
 
 var player: Player
+
+# Put this func here so other states can inherit (grappling, hooked, stunned?)
+func release_chain_and_transition_state():
+	player.chain.release()
+	if player.is_on_floor():
+		finished.emit(GROUNDED)
+	else:
+		finished.emit(AIRBORNE)
+
 
 
 func _ready() -> void:
