72d32975e7ddb10db440400a89a694ce56195e75
Pichun <pichugin.mikhail@gmail.com>
2024-11-09 16:23:52 +0100

    Solved Path 2D for enemy + created Group for additional similar enemies to put on lvls (location in Common)

--- common/linear_path_enemy.gd
+++ common/linear_path_enemy.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-@onready var path_follow = PathFollow2D = $Path2D/PathFollow2D
+@onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
 @export var speed = 100 # px/sec
 
 
