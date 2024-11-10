9950408e238230c25db7389db2b8508d1fb494d6
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-11-11 11:36:57 +1300

    cursor fixes

--- globals/cursor/Cursor.gd
+++ globals/cursor/Cursor.gd
@@ -8,70 +8,54 @@ enum Shapes {
 
 ## Instead of the Input function, use this as it only shows the sprite and never the cursor
 
-@export var sprite: AnimatedSprite2D
 
-@onready var previous_mouse_shape: int = Input.get_current_cursor_shape()
+@onready var sprites = {
+	Shapes.CURSOR_POINTER: $CanvasLayer/pointer,
+	Shapes.CURSOR_SWORD:$CanvasLayer/sword,
+	Shapes.CURSOR_TARGET: $CanvasLayer/target,
+	Shapes.CUSOR_DISABLED: $CanvasLayer/disabled
+}
 
-var shape: int = Input.CURSOR_ARROW: set = set_shape
-var current_animation: StringName
-var current_frame_texture: Texture2D
 
+
+func _ready():
+	for sprite in sprites.values():
+		sprite.hide()
+	
+
+var shape: int = Input.CURSOR_ARROW: 
+	set(value):
+		if shape <= - 1:
+			shape = Input.CURSOR_ARROW
+		shape = value
+		var sprite:Sprite2D = sprites[shape]
+		
+		var hotspot = Vector2.ZERO
+		match shape:
+			Shapes.CURSOR_TARGET:
+				hotspot = sprite.texture.get_size() * 0.5
+			Shapes.CURSOR_POINTER:
+				hotspot = sprite.offset
+		
+		Input.set_custom_mouse_cursor(
+			sprite.texture,
+			Input.get_current_cursor_shape(),
+			hotspot,
+		)
+
+		
 
 var on_grapple_point:bool:
 	get: return shape==Shapes.CURSOR_TARGET
 
 
-func _ready():
-	sprite.hide()
+	
 
-func _process(_delta: float):
-	var current_mouse_shape = Input.get_current_cursor_shape()
+func _physics_process(delta):
+	queue_redraw()
 
-	if current_mouse_shape != previous_mouse_shape:
-		set_shape(current_mouse_shape)
-		previous_mouse_shape = current_mouse_shape
 
-	current_animation = sprite.animation
-	current_frame_texture = sprite.sprite_frames.get_frame_texture(
-			current_animation, sprite.frame
-		)
 
-	Input.set_custom_mouse_cursor(
-		current_frame_texture,
-		Input.get_current_cursor_shape(),
-		sprite.offset
-	)
-
-## Gets the CursorShape in a formatted string without the prefix CURSOR_ Mainly used for changing animations
-func get_shape_name() -> StringName:
-	return Shapes.keys()[shape].to_lower().trim_prefix("cursor_")
-
-func set_shape(value: int) -> void:
-	if !sprite: return
-
-	shape = value
-
-	if shape <= - 1:
-		shape = Input.CURSOR_ARROW
-
-	var anim_name: StringName = get_shape_name()
-	var sprite_frames: SpriteFrames = sprite.sprite_frames
-
-	var has_animation: bool = sprite_frames.has_animation(anim_name)
-
-	if has_animation:
-		sprite.play(anim_name)
-		sprite.offset = get_offset()
-		return
-	else:
-		push_warning("Animation %s does not exist" % anim_name)
-
-func get_offset() -> Vector2:
-	if !current_frame_texture:
-		return Vector2.ZERO
-
-	#match shape:
-		#Input.CURSOR_ARROW, Input.CURSOR_POINTING_HAND, Input.CURSOR_ARROW:
-			#return Vector2.ZERO
-
-	return current_frame_texture.get_size() * 0.5
+func _draw():
+	draw_circle(get_global_mouse_position(), 3, Color.RED)
+	
