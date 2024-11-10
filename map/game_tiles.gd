de0906b6590cb3a8ae18814ce4b54fb34b314b78
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-11-11 11:38:55 +1300

    fix colliders

--- map/game_tiles.gd
+++ map/game_tiles.gd
@@ -6,6 +6,10 @@ var hover_tile_data:TileData = null
 # This is set withing the TileSet resource
 const IS_GRAPPLE_POINT_FLAG = "grapple_point" 
 
+func _ready():
+	if position!=Vector2.ZERO:
+		push_error("tilemap not centerd, errors will aise.")
+
 var __hovering_on_grapple_point:bool:
 	get: 
 		if hover_tile_data and hover_tile_data.get_custom_data(IS_GRAPPLE_POINT_FLAG):
@@ -16,6 +20,7 @@ var __hovering_on_grapple_point:bool:
 func get_hover_tile_data()->TileData:
 	var mouse_pos:Vector2 = get_global_mouse_position()
 	var tile_pos:Vector2 = local_to_map(mouse_pos)
+	
 	return get_cell_tile_data(tile_pos)
 
 func _process(delta):	
