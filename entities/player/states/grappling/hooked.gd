c331c9022524a76e01d4c2a30017d20158622461
Nicolas Samelson <salocin.sam@gmail.com>
2024-11-12 16:47:19 +0100

    fixed grappling states
    - moved code from grappled back to grappling
    - fixed overshooting
    - hooked overwrites the physics to be in an unmovable state until releasing grappling
    - unlinked the swing state for now

--- entities/player/states/grappling/hooked.gd
+++ entities/player/states/grappling/hooked.gd
@@ -5,16 +5,13 @@ extends Grappling
 
 
 func physics_update(delta: float) -> void:
-	var d:float = distance_to_tip
-	
-	chain_velocity =  chain_direction * -player.CHAIN_PULL
-	chain_velocity.y *= 0.55 if chain_velocity.y > 0.0 else 1.65
-	
-
-	player.velocity *= 0.3 # Damping factor
-			
-
-	update_player_velocity()
+	pass
+#
+#
+	#player.velocity *= 0.1 # Damping factor
+			#
+#
+	#update_player_velocity()
 	
 ## Called by the state machine upon changing the active state. The `data` parameter
 ## is a dictionary with arbitrary data the state can use to initialize itself.
--- entities/player/states/grappling/swinging.gd
+++ entities/player/states/grappling/swinging.gd
@@ -1,15 +1,17 @@
 extends Grappling
 
+# Let's not use it for now
+
 const SWING_FORCE = 50							# Force applied to swing while hooked
 
-## Called by the state machine on the engine's physics update tick.
-func physics_update(_delta: float) -> void:
-	
-	
-	player.velocity.x += sign(input_direction.x) * SWING_FORCE
-	player.velocity.y += sign(input_direction.y) * SWING_FORCE / 5
-	
-	update_player_velocity()
+### Called by the state machine on the engine's physics update tick.
+#func physics_update(_delta: float) -> void:
+	#
+	#
+	#player.velocity.x += sign(input_direction.x) * SWING_FORCE
+	#player.velocity.y += sign(input_direction.y) * SWING_FORCE / 5
+	#
+	#update_player_velocity()
 
 func enter(previous_state_path: String, data := {}) -> void:
 	pass
