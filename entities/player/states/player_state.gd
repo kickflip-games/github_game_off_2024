0c867aacebb7686d1c571e5b9b2a1a85c84bffce
Nicolas Samelson <salocin.sam@gmail.com>
2024-11-12 16:53:02 +0100

    removed unused states in playerstates

--- entities/player/states/player_state.gd
+++ entities/player/states/player_state.gd
@@ -1,28 +1,23 @@
 class_name PlayerState extends State
 
 const GRAPPLING = "Grappling"
-const GRAPPLED = "Grappling/Grappled"
 const HOOKED = "Grappling/Hooked"
-const SWINGING = "Grappling/Swinging"
-const GRAPPLE_STATES = [GRAPPLING, HOOKED, SWINGING,GRAPPLED]
+#const SWINGING = "Grappling/Swinging"
+const GRAPPLE_STATES = [GRAPPLING, HOOKED]
 
 const GROUNDED = "Grounded"
 const IDLE = "Grounded/Idle"
 const WALKING = "Grounded/Walking"
-const GROUNDED_STATES = [GROUNDED, IDLE, WALKING]
+const GROUNDED_STATES = [IDLE, WALKING]
 
 const AIRBORNE = "Airborne"
 const JUMPING = "Airborne/Jumping"
 const FALLING = "Airborne/Falling"
-const AIRBORNE_STATES = [AIRBORNE,JUMPING, FALLING]
+const AIRBORNE_STATES = [JUMPING, FALLING]
 
 const ATTACKING = "Attacking"
 const DEAD = "Dead"
 
-#TODO: add these other states?
-#const DANCING = "Dancing"
-#const STUNNED = "Stunned"
-#const INJURED = "Injured" # or hit?
 
 
 var player: Player
