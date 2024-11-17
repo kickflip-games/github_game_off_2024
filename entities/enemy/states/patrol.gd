c652470edc96eda0433b9be2155958ba340492fd
Pichun <pichugin.mikhail@gmail.com>
2024-11-17 17:36:26 +0100

    Added randomness while patrolling

--- entities/enemy/states/patrol.gd
+++ entities/enemy/states/patrol.gd
@@ -9,7 +9,13 @@ extends EnemyState
 
 var current_point_index: int = 0    # Index of the current target point
 var target_point: Vector2             # Current target point to move towards
+var move_direction : Vector2 	#initialize random patrol direction
+var wander_time : float # initialize random time of patrol before changing direction
 
+
+func randomize_wander(): # randomize direction and time when patrolling
+	move_direction = Vector2(randf_range(-1,1),randf_range(-1,1)).normalized()
+	wander_time = randf_range(1,3)
 
 
 func update(_delta: float) -> void:
@@ -17,8 +23,13 @@ func update(_delta: float) -> void:
 		finished.emit(ALERTED)
 	if enemy.isDead:
 		finished.emit(DEATH)
+	if wander_time > 0 :
+		wander_time -= _delta
+	else:
+		randomize_wander()
 
 func physics_update(_delta: float) -> void:
+	# if random patrol, then random beahavior patrol, else follow path
 	move_along_path(_delta)
 	
 func move_along_path(_delta: float) -> void:
@@ -28,7 +39,7 @@ func move_along_path(_delta: float) -> void:
 	elif enemy.direction < 0 and enemy.path_follow.progress_ratio == 1:
 		finished.emit(IDLE)
 	else:
-		var dx = enemy.direction * enemy.walk_speed * _delta
+		var dx = move_direction.x * enemy.walk_speed * _delta
 		enemy.path_follow.set_progress(enemy.path_follow.get_progress() + dx)	
 	
 	
