d86c759279db4047dc96ee4e6c1e18072bc5664d
Nicolas Samelson <salocin.sam@gmail.com>
2024-11-28 17:37:14 +0100

    Changed grappling to a staticbody
    BIG CHANGES:
    - instead of a specific tile, which I had difficulty finding the center (new godot version changed stuff), it looks at staticbody "HookablePoint".
    - it has a detection zone, and a small collision
    - however need to put the points manually for now

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -10,12 +10,12 @@ const FRICTION_AIR = 0.95
 const FRICTION_GROUND = 0.85
 const CHAIN_PULL = 105.0
 const HOOK_DIRECTION_OFFSET = 0.05				# Vertical offset for the hook direction
-const MAX_HOOK_DISTANCE = 500.0  				# Maximum distance the hook can extend TODO: remove it
+const MAX_HOOK_DISTANCE = 250.0  				# Maximum distance the hook can extend TODO: remove it
 const AT_HOOK_TIP_DISTANCE = 25					# Threshold to dampen when close to the tip
 const MIN_GRAPPLE_DIST = 0.1
 const ATTACK_FORCE = 500
 
-const RAYCAST_LENGTH = 500
+const RAYCAST_LENGTH = 250
 
 # Variables
 var chain_velocity: Vector2 = Vector2.ZERO
@@ -107,7 +107,29 @@ func get_grapple_input_vector(_event: InputEvent)->Vector2:
 	# 		direction.y -= HOOK_DIRECTION_OFFSET # vertical offset if hook aims too low
 	# return direction
 
-	# VERSION 3: checking the direction is aimed at grapple-able tile
+	## VERSION 3: checking the direction is aimed at grapple-able tile
+	#var mouse_pos: Vector2 = get_global_mouse_position()
+	#direction = (mouse_pos - global_position).normalized()
+	#raycast_grapple.target_position = direction * RAYCAST_LENGTH
+	#raycast_grapple.force_raycast_update()
+#
+	## check if raycast hits a grappable tile
+	#if raycast_grapple.is_colliding() and _event is InputEventMouseButton:
+		#if _event.is_pressed() and  !enemies_are_nearby: # Mouse button down
+			#var collider = raycast_grapple.get_collider()
+			#
+			#if collider and collider is TileMapLayer:
+				#var collision_point: Vector2 = raycast_grapple.get_collision_point()
+				#var tile_pos: Vector2 = collider.local_to_map(collision_point)
+				#var tile_data: TileData = collider.get_cell_tile_data(tile_pos)
+#
+				## Check if this tile has the "grapple_point" flag or custom metadata
+				#if tile_data and tile_data.get_custom_data("grapple_point"):
+					#print("Grappable point detected at:", tile_pos)
+					#return direction
+
+
+	# VERSION 4: checking the direction is aimed at grapple-able tile
 	var mouse_pos: Vector2 = get_global_mouse_position()
 	direction = (mouse_pos - global_position).normalized()
 	raycast_grapple.target_position = direction * RAYCAST_LENGTH
@@ -118,16 +140,12 @@ func get_grapple_input_vector(_event: InputEvent)->Vector2:
 		if _event.is_pressed() and  !enemies_are_nearby: # Mouse button down
 			var collider = raycast_grapple.get_collider()
 			
-			if collider and collider is TileMapLayer:
-				var collision_point: Vector2 = raycast_grapple.get_collision_point()
-				var tile_pos: Vector2 = collider.local_to_map(collision_point)
-				var tile_data: TileData = collider.get_cell_tile_data(tile_pos)
 
-				# Check if this tile has the "grapple_point" flag or custom metadata
-				if tile_data and tile_data.get_custom_data("grapple_point"):
-					print("Grappable point detected at:", tile_pos)
-					return direction
-
+			if collider and collider is StaticBody2D and collider.name == "DetectionBody2D":
+				var grapple_center: Vector2 = collider.global_position
+				print("Raycast collision point:", raycast_grapple.get_collision_point())
+				print("Collider global position (center):", collider.global_position)
+				return (grapple_center - global_position).normalized()
 	# if no collision with grappable tile			
 	return Vector2.ZERO
 
